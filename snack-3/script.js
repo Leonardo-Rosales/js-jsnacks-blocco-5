8edc1d16b256d25e068e31e287af23aeec5ce36e
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-10-30 15:48:45 +0100

    ADD snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,14 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersIncrementedByOne = []
+
+numbers.forEach((num) => {
+	const increase = num + 1
+	numbersIncrementedByOne.push(increase)
+})
+
+console.log(numbersIncrementedByOne)
+
+
